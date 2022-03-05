dcc10deca976ec3c31bf322eee37c1a062b1e3d9
Archana9814 <archana.arora25@gmail.com>
2022-03-05 20:34:20 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket        = "my-s3-bucket"
-  bucket_prefix = "archanabenarora"
+  bucket        = "archanabenarora-my-s3-bucket"
   acl           = "private"
 
   versioning = {
